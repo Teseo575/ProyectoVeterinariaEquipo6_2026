950614cee4fc5b79d1f36465bee9cda405903e6a
Nahuel Ruiz <nahuel.elinteligente11@gmail.com>
2026-09-02 10:30:31 -0300

    Borrar version vieja del codigo proyecto
    Actualización general del codigo

--- proyecto/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Abre la conexion con MySQL usando la base de datos del proyecto.
-$con = new mysqli('localhost', 'root', '', 'bd_veterinaria');
-
-// Permite guardar y leer correctamente tildes y otros caracteres especiales.
-$con -> set_charset("utf8");
-?>
